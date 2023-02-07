e53b17cdf7693fe90a75e02eb864c8618c588c2f
gyuturn <rbtjrrbals@gmail.com>
2023-02-07 23:10:14 +0900

    #40 feat(): cookie domain 설정 local

---
+++ backend/src/main/java/com/kaspi/backend/util/cookie/CookieSerialize.java
@@ -0,0 +1,23 @@
+package com.kaspi.backend.util.cookie;
+
+import java.net.MalformedURLException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
+
+@Configuration
+@Slf4j
+public class CookieSerialize {
+
+    @Bean
+    public CookieSerializer cookieSerializer() throws MalformedURLException {
+        log.info("cookie fileter");
+        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+        serializer.setDomainName("localhost");
+        serializer.setUseSecureCookie(true);
+        return serializer;
+    }
+}
+
